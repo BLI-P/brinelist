09aae83981cd3f21122da67b49e7341f246b5ee8
BLI-P <shivankpandey.programmer@gmail.com>
2022-12-30 08:25:47 +0530

    Initial Commit

--- src/configs/altogic.js
+++ src/configs/altogic.js
@@ -1,8 +1,8 @@
 import { createClient } from "altogic";
 
 // This `envUrl` and `clientKey` is sample you need to create your own.
-const envUrl = "https://online-shopping-list.c1-europe.altogic.com";
-export const clientKey = "419ab1e3b2f14e44bd7c8be9f08468d4";
+const envUrl = "https://1vd3-snum.c1-india.altogic.com";
+export const clientKey = "278749ce2a4c472d8dcf3c0c0047a2c2";
 
 const altogic = createClient(envUrl, clientKey, {
   // signInRedirect: "/sign-in",
